c8cfad5800f1b31b9279e9bc5ab9c8a2920bcae2
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-03-21 14:28:09 +0100

    Add files via upload

---
+++ second.py
@@ -0,0 +1,67 @@
+from threading import Thread
+import heapq
+import time
+
+"""
+5 - Tupel
+
+(Ereigniszeipunkt
+Ereignispriorität
+Ereignisnummer
+Ereignisfunktion
+Args)
+
+"""
+
+
+class Event():
+    def __init__(self,
+                 Ereigniszeipunkt,
+                 Ereignispriorität,
+                 Ereignisnummer,
+                 Ereignisfunktion,
+                 Args):
+        self.EventList = []
+
+    def push(Event):
+        pass
+
+    def start():
+        pass
+
+
+class Station():
+    def __init__(self, Bediendauer):
+        pass
+
+    def anstellen(Kunde):
+        pass
+
+    def fertig(Kunde):
+        pass
+
+
+"""
+3 - Tupel 
+
+noch zu besuchende Stationen
+
+"""
+
+
+class Kunde():
+    def __init__(self, WarteZeit, Strecke, Anzahl):
+        pass
+
+    def begin_einkauf():
+        pass
+
+    def ankunft_station():
+        pass
+
+    def verlassen_station():
+        pass
+
+
+a = Kunde("Peter")
+b = Event("Supermarkt")
